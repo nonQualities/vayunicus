d4174092226e206f940713465043914ee565d76a
nonQualities <ronit965choudhury@outlook.com>
2025-12-27 13:21:48 +0530

    Remove cmake and test folders from repository

--- cmake/install-rules.cmake
+++
@@ -1,8 +0,0 @@
-install(
-    TARGETS vayunicus_exe
-    RUNTIME COMPONENT vayunicus_Runtime
-)
-
-if(PROJECT_IS_TOP_LEVEL)
-  include(CPack)
-endif()
--- test/source/vayunicus_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-
-#include "lib.hpp"
-
-TEST_CASE("Name is vayunicus", "[library]")
-{
-  auto const lib = library {};
-  REQUIRE(lib.name == "vayunicus");
-}
